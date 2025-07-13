6427d0d2e58e0c1bc297d323a5d9f897acf563f2
jebin2 <jebineinstein@gmail.com>
2025-07-13 17:57:43 +0530

    def briwser if not

--- browser_manager/local_browser_launcher.py
+++ browser_manager/local_browser_launcher.py
@@ -53,7 +53,7 @@ class LocalBrowserLauncher(BrowserLauncher):
             config.browser_executable,
             f"--remote-debugging-port={config.debugging_port}",
             f"--user-data-dir={config.user_data_dir}",
-            config.chrome_flags
+            config.chrome_flags.split()
         ]
         
         if config.headless:
