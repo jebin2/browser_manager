2d1d7d2d4e8585181244db5c3281264faef80050
jebin2 <jebineinstein@gmail.com>
2026-03-02 01:20:55 +0530

    refactor: remove page_actions module and its imports from browser_manager.

--- browser_manager/__init__.py
+++ browser_manager/__init__.py
@@ -1,8 +1,1 @@
 from .browser_manager import BrowserManager
-from .page_actions import (
-    find_and_highlight_element,
-    remove_highlights,
-    scroll_to_element,
-    scroll_continuous,
-    capture_viewport_frames
-)
